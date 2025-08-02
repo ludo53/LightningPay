bbfada08887a4260d1954e16ffb9bf8f783bb569
Ludo <97764715+ludo53@users.noreply.github.com>
2025-08-03 00:26:24 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/lightningpay/main.go
@@ -0,0 +1,23 @@
+// cmd/lightningpay/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"lightningpay/internal/lightningpay"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := lightningpay.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
